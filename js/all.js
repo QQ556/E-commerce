ad3815730f419232c211ea604e6215a24374c44e
QQ556 <34880909+QQ556@users.noreply.github.com>
2019-02-18 18:04:57 +0800

    註冊帳號功能-0218

--- js/all.js
+++ js/all.js
@@ -2,3 +2,7 @@ function closebar() {
             var spanObj = document.getElementById("emotion");
             spanObj.style.display = "none"
         }
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+    })
