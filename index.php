2144aa5ec1581bbc1faa64fde2c02335b53ec734
QQ556 <34880909+QQ556@users.noreply.github.com>
2019-02-19 20:48:58 +0800

    2

--- index.php
+++ index.php
@@ -18,7 +18,25 @@
         integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
         crossorigin="anonymous"></script>
     <script src="js\all.js">
+    (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
     </script>
+
     <link rel="stylesheet" type="text/css" href="css\main.css">
     </div>
 </head>
@@ -50,7 +68,7 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                    <form>
+    <form id="needs-validation" novalidation>
   <div class="form-row">
     <div class="col-md-4 mb-3">
       <label for="validationServer01">First name</label>
@@ -115,7 +133,7 @@
   </div>
   <button class="btn btn-primary" type="submit">Submit form</button>
 </form>
-                    </div>
+              </div>
                     <div class="modal-footer">
                         
                     </div>
