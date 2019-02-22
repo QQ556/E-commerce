b6c5d36ddb2ad3accf96ebda1966401e94e86324
QQ556 <34880909+QQ556@users.noreply.github.com>
2019-02-22 16:15:10 +0800

    0222

--- js/all.js
+++ js/all.js
@@ -1,12 +1,15 @@
 //通知列關閉
-function closebar() {
-    var spanObj = document.getElementById("emotion");
-    spanObj.style.display = "none"
-}
-//model 啟用
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+// function closebar() {
+//     var spanObj = document.getElementById("emotion");
+//     spanObj.style.display = "none"
+// }
+$(document).ready(function () {
+    $(".closebar").click(function(){
+        $(".alert").slideUp(1000);
+    });
+});
+
+
 //註冊資料判斷
 function EmailChecking() {
     var Obj = document.getElementById("exampleInputEmail1");
@@ -18,7 +21,7 @@ function EmailChecking() {
         if ((i - temp) > 3) {
             if ((i - tempd) > 0) {
                 console.log("信箱對");
-                document.getElementById("emailHelp").innerHTML = "信箱可以使用 !! ";
+                document.getElementById("emailHelp").innerHTML = "看起來不錯 !! ";
                 document.getElementById("emailHelp").style.color = "green";
                 return 1;
             }
@@ -30,11 +33,12 @@ function EmailChecking() {
     return 0;
 }
 //密碼資料判段
+var Check = 0 ;
 function PasswordChecing() {
     var Obj = document.getElementById("exampleInputPassword1");
     var pwd = Obj.value;
     var i = pwd.length;
-    if (pwd.match(/\d/) && pwd.match(/[a-z]/i) && i > 8 && i < 20 && !pwd.match(/[^a-z0-9]/)) {
+    if (pwd.match(/\d/) && pwd.match(/[a-z]/i) && i > 7 && i < 20 && !pwd.match(/[^a-z0-9]/)) {
         console.log("yes");
         document.getElementById("passwordHelp").innerHTML = "密碼可以使用!!";
         document.getElementById("passwordHelp").style.color = "green";
@@ -49,30 +53,11 @@ function RePassword() {
     var pwd1 = Obj1.value;
     var Obj2 = document.getElementById("exampleInputPassword2");
     var pwd2 = Obj2.value;
-    if (condition) {
-        if (pwd1 = pwd2);
+        if (pwd1 == pwd2){
         console.log("密碼驗證正確!");
     }
-    else {
+    else{
         console.log("密碼驗證錯誤!");
     }
 
 }
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
---
+++ mail.php
@@ -0,0 +1,8 @@
+<?php
+    $emailtTo = "a700mb0000@gmail.com";
+    $tittle = "text mail";
+    $body = "hihihihihihih";
+    $headers="From:myemail@gmail.com";
+
+    mail($emailtTo,$tittle,$body,$headers);
+?>
